7166ac055308e4219e230142bc6cf1514af29c31
Dmitry <56270217+DavisDmitry@users.noreply.github.com>
2021-10-28 18:05:42 +0500

    🐛 Fix generate_uuid util (#7)

--- cubes/utils.py
+++ cubes/utils.py
@@ -6,8 +6,7 @@ import uuid
 def generate_uuid(player_name: str) -> uuid.UUID:
     """Generates UUID by player_name for using in offline mode."""
 
-    class NameSpace:
-        # pylint: disable=C0115
-        bytes = b"OfflinePlayer"
+    class _NameSpace:
+        bytes = b"OfflinePlayer:"
 
-    return uuid.uuid3(NameSpace, player_name)
+    return uuid.uuid3(_NameSpace, player_name)
