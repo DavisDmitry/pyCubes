5f0cd415e4448059ab83d943d57f6d3cba0cfa8c
DavisDmitry <carlversalski@gmail.com>
2021-10-31 20:41:14 +0500

    🏷️ Add PlayerData type

--- cubes/__init__.py
+++ cubes/__init__.py
@@ -1,4 +1,4 @@
 from cubes.app import Application
 from cubes.buffer import ReadBuffer, WriteBuffer
 from cubes.connection import CloseConnection, PlayerConnection
-from cubes.types import ConnectionStatus
+from cubes.types import ConnectionStatus, PlayerData
