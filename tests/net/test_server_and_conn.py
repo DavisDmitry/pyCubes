e99b7d78dd99944655cfa2892900c6ad3f12eae5
DavisDmitry <carlversalski@gmail.com>
2021-11-28 23:21:10 +0500

    Add test for net module

---
+++ tests/net/test_server_and_conn.py
@@ -0,0 +1,81 @@
+import io
+from typing import Callable, Iterator
+
+import anyio
+import anyio.abc
+import pytest
+
+from cubes import net
+
+pytestmark = pytest.mark.anyio
+
+
+_HOST = "127.0.0.1"
+_PORT = 25560
+
+
+@pytest.fixture
+async def server() -> Iterator[tuple[dict[str, bool], Callable]]:
+    stats = {"new_conn": False, "timeout": False, "close_conn": False}
+
+    async def _process_new_conn(_):
+        stats["new_conn"] = True
+
+    async def _process_packet_receive_timeout(conn: net.Connection):
+        stats["timeout"] = True
+        await conn.close()
+
+    async def _process_packet(conn: net.Connection, packet: io.BytesIO):
+        await conn.send(packet)
+
+    async def _process_close_conn(_, __):
+        stats["close_conn"] = True
+
+    server = net.Server(
+        _process_new_conn,
+        _process_packet_receive_timeout,
+        _process_packet,
+        _process_close_conn,
+        packet_receive_timeout=0.25,
+    )
+
+    async def _task(task_status: anyio.abc.TaskStatus = anyio.TASK_STATUS_IGNORED):
+        task_status.started()
+        await server.run(_HOST, _PORT)
+
+    assert not server.is_running
+    yield stats, _task
+
+
+async def test_server(server: tuple[dict[str, bool], Callable]):
+    stats, task = server
+    data = b"\x01\x00"
+
+    async with anyio.create_task_group() as task_group:
+        await task_group.start(task)
+        await anyio.sleep(0.01)
+
+        async with await anyio.connect_tcp(_HOST, _PORT) as stream:
+            await stream.send(data)
+            assert await stream.receive() == data
+
+        async with await anyio.connect_tcp(_HOST, _PORT):
+            await anyio.sleep(0.5)
+
+        task_group.cancel_scope.cancel()
+
+    assert {"new_conn": True, "timeout": True, "close_conn": True} == stats
+
+
+async def test_conn(server: tuple[dict, Callable]):
+    _, task = server
+    async with anyio.create_task_group() as task_group:
+        await task_group.start(task)
+        await anyio.sleep(0.01)
+        async with await anyio.connect_tcp(_HOST, _PORT) as stream:
+            conn = net.Connection(stream)
+            conn.status = net.ConnectionStatus.LOGIN
+            assert conn.status == net.ConnectionStatus.LOGIN
+            conn.remote_address
+            conn.local_address
+        task_group.cancel_scope.cancel()
