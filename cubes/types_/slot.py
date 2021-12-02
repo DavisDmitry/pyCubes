abafd301a88c7b5a2562ca8c15830c104f2a74ec
DavisDmitry <carlversalski@gmail.com>
2021-12-02 18:18:58 +0500

    ⚡️ Upgrade slot type validation

--- cubes/types_/slot.py
+++ cubes/types_/slot.py
@@ -2,28 +2,26 @@ from typing import Optional
 
 import pydantic
 
-from cubes import nbt as nbt_
-from cubes.net.serializers import _nbt, _var_length
+from cubes import nbt as _nbt
 
 
 class Slot(pydantic.BaseModel):
     item_id: int = pydantic.Field(gt=0, allow_mutation=False)
     count: int = pydantic.Field(gt=0, le=64)
-    nbt: Optional[nbt_.Compound]
+    nbt: Optional[_nbt.Compound]
 
-    def __init__(self, item_id: int, count: int, *, nbt: nbt_.Compound | None = None):
+    def __init__(
+        self, item_id: int, count: int, *, nbt: _nbt.Compound | dict | None = None
+    ):
         super().__init__(item_id=item_id, count=count, nbt=nbt)
-
-    @pydantic.validator("item_id")
-    @staticmethod
-    def _validate_item_id(value: int):
-        _var_length.VarIntSerializer.validate(value)
 
     @pydantic.validator("nbt")
     @staticmethod
-    def _validate_nbt(value: nbt_.Compound | None):
-        if value is not None:
-            _nbt.NBTSerializer.validate(value)
+    def validate_nbt(value: _nbt.Compound | None):
+        if value is None:
+            return value
+        return _nbt.Compound(value)
 
     class Config:
+        # pylint: disable=R0903
         validate_assignment = True
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,5 +1,6 @@
 import pydantic
 import pytest
+from pydantic.class_validators import validator
 
 from cubes import nbt as _nbt
 from cubes import types_
@@ -7,10 +8,13 @@ from cubes import types_
 
 @pytest.mark.parametrize(
     ("item_id", "count", "nbt"),
-    ((1, 64, None), (272, 1, _nbt.Compound({"Unbreakable": _nbt.Byte(True)}))),
+    ((1, 64, None), (719, 1, _nbt.Compound({"Unbreakable": _nbt.Byte(True)}))),
 )
 def test_valid_slot(item_id: int, count: int, nbt: _nbt.Compound | None):
-    types_.Slot(item_id, count, nbt=nbt)
+    value = types_.Slot(item_id, count, nbt=nbt)
+    assert value.item_id == item_id
+    assert value.count == count
+    assert value.nbt == nbt
 
 
 @pytest.mark.parametrize(
@@ -23,7 +27,11 @@ def test_invalid_slot(item_id: int, count: int, nbt: _nbt.Compound | None):
 
 
 def test_slot_mutation():
+    new_count, new_nbt = 32, _nbt.Compound({"test": _nbt.String("test")})
     slot = types_.Slot(1, 64)
     with pytest.raises(TypeError):
         slot.item_id = 2
-    slot.count = 32
+    slot.count = new_count
+    slot.nbt = new_nbt
+    assert slot.count == new_count
+    assert slot.nbt == new_nbt
